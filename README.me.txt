6ade16fe56fd8a6d34ba1e8333dd6164316c2e03
Gergely <murvai.gergely@gmail.com>
2018-12-17 13:57:52 +0100

    Basic .net Web Api works

---
+++ README.me.txt
@@ -0,0 +1,9 @@
+First attempt to make a .NET Core Web API 
+
+This project is based on Microsoft's tutorial:
+https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?=aspnetcore-2.2&tabs=visual-studio
+
+As I finish this tutorial, I would like to extend the following project with:
+	-PostgreSQL database instead of In Memory;
+	-Simple database schema with more entities;
+	-User login session;
---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : Controller
+    {
+
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.TodoItems.Add(new TodoItem { Name = "Item2" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET: api/Todo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+        }
+
+        // PUT: api/Todo/5
+        // Returns 201 when object modified, and 400 when ids are'nt matching
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest("Id mismatch");
+            }
+
+            try
+            {
+                var oldTodoItem = _context.TodoItems.Where(t => t.Id == todoItem.Id)
+                                                    .FirstOrDefault<TodoItem>();
+                _context.Entry(oldTodoItem).State = EntityState.Detached;
+                _context.Entry(todoItem).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            }
+            catch (DbUpdateConcurrencyException dbEx)
+            {
+                return BadRequest("DbUpdateConcurrencyException :\n"+dbEx.Message);
+            }
+            catch (InvalidOperationException invalid)
+            {
+                return BadRequest("InvalidOperationException :\n" + invalid.Message);
+            }
+        }
+
+        // DELETE: api/Todo/5
+        // Returns 204 when transaction is successful, 404 when item not found
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TodoItem>> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
